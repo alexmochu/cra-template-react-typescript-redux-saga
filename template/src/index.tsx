6f2b61afae4e5d54bae39b6c09a1cf82e445fea3
alexmochu <mochualex4@gmail.com>
2020-07-14 13:27:20 +0300

    create template

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import configureStore from './store'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const initialState = {};
+const store = configureStore(initialState);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
